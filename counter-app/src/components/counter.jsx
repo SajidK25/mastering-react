3a983cb2d2fa58a0be995183804095c7e369a779
Sajid Khan <sajidk25@gmail.com>
2020-04-30 16:29:00 +0600

    Renderring Classes Dynamically

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,28 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imgUrl: "http://image.com/200",
+    count: 1,
   };
-  style = {
-    fontSize: 20,
-    fontWeight: "bold",
-  };
+
   render() {
     return (
       <React.Fragment>
-        {/* <image src={this.state.imgUrl}></image> */}
-        {/* <span style={this.style} className="badge badge-primary m-2"> */}
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increament</button>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count == 0 ? "Zero" : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
