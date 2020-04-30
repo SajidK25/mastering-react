0a563df5dfdbbcaa958b7c96f6bfaeb75a56e203
Sajid Khan <sajidk25@gmail.com>
2020-04-30 15:27:06 +0600

    Specifying children

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,7 +3,12 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {};
   render() {
-    return <h1>Hello world!!</h1>;
+    return (
+      <React.Fragment>
+        <h1>Hello world!!</h1>
+        <button>Increament</button>
+      </React.Fragment>
+    );
   }
 }
 
