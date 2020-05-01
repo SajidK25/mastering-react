181f1ab82ab432af7e1435938f9896f88f4c841f
Sajid Khan <sajidk25@gmail.com>
2020-05-01 23:48:35 +0600

    Deleting a Movie

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
   return (
     <main role="main" className="container">
-      Hello World
+      <Movies />
     </main>
   );
 }
