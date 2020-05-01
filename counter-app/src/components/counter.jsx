a72e4a14c88ad3bb3f9201c74f59cc1633d65cd1
Sajid Khan <sajidk25@gmail.com>
2020-05-01 14:27:24 +0600

    Handling Events

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,37 +6,32 @@ class Counter extends Component {
     tags: [],
   };
 
-  renderList() {
-    if (this.state.tags.length === 0) return <h1>There are no tags</h1>;
-    else
-      return (
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      );
+  handleIncreament() {
+    console.log("increament clicked", this);
   }
   render() {
     return (
       <React.Fragment>
-        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increament</button> */}
-        {this.state.tags.length === 0 && "Plz insert some tags"}
-        <ul>{this.renderList()}</ul>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncreament}
+          className="btn btn-secondary btn-sm"
+        >
+          Increament
+        </button>
       </React.Fragment>
     );
   }
-  //   getBadgeClasses() {
-  //     let classes = "badge m-2 badge-";
-  //     classes += this.state.count === 0 ? "warning" : "primary";
-  //     return classes;
-  //   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-  //   formatCount() {
-  //     const { count } = this.state;
-  //     return count === 0 ? "Zero" : count;
-  //   }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
