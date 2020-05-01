a11e4677cafdc7a7a582b8c475a420d0547f5e3f
Sajid Khan <sajidk25@gmail.com>
2020-05-01 15:34:24 +0600

    Setting up Vidly project

---
+++ vidly/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main role="main" className="container">
+      Hello World
+    </main>
+  );
+}
+
+export default App;
