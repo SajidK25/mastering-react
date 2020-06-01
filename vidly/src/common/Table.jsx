a122397547e6da73ec705a5eeccfec5ced720ea7
Sajid Khan <sajidk25@gmail.com>
2020-06-01 21:01:21 +0600

    Sorting - Destructuring Arguments

--- vidly/src/common/Table.jsx
+++ vidly/src/common/Table.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import TablHeader from "./TableHeader";
 import TableBody from "./TableBody";
 
-const Table = (props) => {
-  const { columns, sortColumn, onSort, data } = props;
+const Table = ({ columns, sortColumn, onSort, data }) => {
   return (
     <table className="table">
       <TablHeader columns={columns} onSort={onSort} sortColumn={sortColumn} />
