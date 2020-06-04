79855c77e887773198640b8a3a9bcf477bc39de3
Sajid Khan <sajidk25@gmail.com>
2020-06-04 19:31:44 +0600

    Adding the Navbar

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -5,19 +5,23 @@ import Movies from "./components/movies";
 import Customers from "./components/cutomers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./common/navbar";
 
 function App() {
   return (
-    <main role="main" className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main role="main" className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
