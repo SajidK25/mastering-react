354bcff9e486d9f5002168b5b0253ce8d827cc0e
Sajid Khan <sajidk25@gmail.com>
2020-06-06 12:22:48 +0600

    Handling  Form Submission

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,21 +1,29 @@
-import React from "react";
-const LoginForm = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div className="from-group">
-          <label htmlFor="username">User Name</label>
-          <input id="username" type="text" className="form-control" />
-        </div>
-        <div className="from-group">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="text" className="form-control" />
-        </div>
-        <button className="btn btn-primary">Login</button>
-      </form>
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //call server
+    console.log("submitted");
+  };
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="from-group">
+            <label htmlFor="username">User Name</label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="from-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default LoginForm;
